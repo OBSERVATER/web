64b0db9cb7487eaca6273e1ff22af988b4e9c5b6
OBSERVATER <3254140082@qq.com>
2024-11-14 23:59:22 +0800

    change app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def run_script():
     return jsonify({'output': "输出:"+result.stdout})
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run()
